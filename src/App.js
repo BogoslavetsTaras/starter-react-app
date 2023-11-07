5ee63b00d4791e9c5cd47984b2d55e0ca7344440
Taras Bohoslavets <106775522+BogoslavetsTaras@users.noreply.github.com>
2023-11-07 21:10:12 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            <b>Bohoslavets Taras.</b>
-          
+            <em>Bohoslavets Taras.</em>
         </p>
         <a
           className="App-link"
